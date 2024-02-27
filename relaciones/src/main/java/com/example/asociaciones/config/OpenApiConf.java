bbef01f1660e5b65eb7a5a96ea94fabb15fabcab
RTM841 <rtejmar841@g.educaand.es>
2024-02-27 13:49:26 +0100

    Funciona todo, he comprobado la funcionalidad de todos los roles, y de las consultas, todo funciona de manera correcta. Falta el controlador de canciones y el scrip con los datos. Y manejar excepciones, Ya se puede autenticar en Swagger

--- relaciones/src/main/java/com/example/asociaciones/config/OpenApiConf.java
+++ relaciones/src/main/java/com/example/asociaciones/config/OpenApiConf.java
@@ -1,9 +1,12 @@
 package com.example.asociaciones.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +16,19 @@ http://localhost:8080/swagger-ui/index.html
 */
 @Configuration
 public class OpenApiConf {
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 
     @Bean
     public  OpenAPI openAPI(){
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info()
                         .title("Example API")
                         .description("API REST para una app de música"));
